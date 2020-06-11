13197da4509fd45a9a9b9f3bcb0bfda7983a4b1f
unknown <maiphong27@gmail.com>
2020-06-11 19:14:56 +0700

    up_hienthiThongTinSV

--- models/danhSachSV.js
+++ models/danhSachSV.js
@@ -2,4 +2,7 @@ var DanhSachSinhVien = function(){
     this.capNhatSinhVien = function(){
         console.log('cap nhat danh sach sinh vien');
     }
+    this.hienThiThongTinSv = function(){
+        console.log('Hien thi Thong Tin Sinh Vien');
+    }
 }
